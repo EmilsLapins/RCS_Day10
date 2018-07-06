31982065227201464205d366a87884cd521536c2
Emils Lapins <emilslapins@gmail.com>
2018-07-06 20:03:40 +0300

    Calc frame created.

---
+++ Pogas/Pogas/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pogas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeLabel (string cipars)
+        {
+            label1.Text = cipars;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int a = 5;
+            string Ievade = button1.Text;
+            ChangeLabel(Ievade);
+
+            //label1.Text = Ievade;
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Ievade = button5.Text;
+            ChangeLabel(Ievade);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Ievade = button2.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Ievade = button3.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string Ievade = button4.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string Ievade = button6.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string Ievade = button7.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string Ievade = button8.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string Ievade = button9.Text;
+            ChangeLabel(Ievade);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string Ievade = button10.Text;
+            ChangeLabel(Ievade);
+        }
+    }
+}
+// Sataisit pogas no 0 - 9 , nospiezot pogu paradas sis cipars!
